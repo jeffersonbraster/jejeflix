193863f32b6a40403e330825e43e6d989578ae87
Jefferson <jeferson.oliveira@g.globo>
2022-04-18 13:33:14 -0300

    iniciando a pagina de movie

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -1,5 +1,59 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import "./style.css";
+
 const Movie = () => {
-  return <div>Movie</div>;
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadMovie() {
+      await api
+        .get(`movie/${id}`, {
+          params: {
+            api_key: process.env.REACT_APP_TMDB_API_KEY,
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setMovie(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("n encontrado");
+        });
+    }
+
+    loadMovie();
+
+    return () => {
+      console.log("desmontado");
+    };
+  }, [id]);
+
+  if (loading) {
+    return (
+      <div className="movie-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="movie-info">
+      <h1>{movie.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt={movie.title}
+      />
+      <h3>Sinopse</h3>
+      <span>{movie.overview}</span>
+
+      <strong>Avaliação: {movie.vote_average} / 10</strong>
+    </div>
+  );
 };
 
 export default Movie;
